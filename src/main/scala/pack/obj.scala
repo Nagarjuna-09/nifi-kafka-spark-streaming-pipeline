bf08eddd51ca4b216819b35f57afa3284ce38e55
Nagarjuna-09 <nagarjunanathani434@gmail.com>
2024-01-26 13:20:18 -0500

    DF conversion and attaches timestamp column for every input message

--- src/main/scala/pack/obj.scala
+++ src/main/scala/pack/obj.scala
@@ -1,10 +1,12 @@
 package pack
 
-import org.apache. spark.SparkConf
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql. functions._
+import org.apache.spark.sql.functions._
 
+//can refer to this website for base template
+//https://spark.apache.org/docs/2.2.0/streaming-kafka-0-10-integration.html
 //imports for spark streaming  + kafka integration  --- 1
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
@@ -13,7 +15,6 @@ import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 //contains StreamingContext class and Seconds() --- 2
-
 import org.apache.spark.streaming._
 
 object obj {
@@ -23,39 +24,51 @@ object obj {
     //required for writing the file from eclipse
     System.setProperty("hadoop.home.dir","D:\\hadoop")
     
-//Spark code required for transforming read data
-    
     val conf = new SparkConf().setAppName("first").setMaster("local[*]").set("spark.driver.allowMultipleContexts", "true")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
     
-    val spark = SparkSession.builder().getOrCreate()
+    val spark = SparkSession.builder().getOrCreate( )
     import spark.implicits._
     
-    // initializing the streaming context ----3
+    //kafka integration with spark starts -----------------------
+    
+    // initializing the streaming context and set for how many seconds spark hits kafka ---- 3
     val ssc = new StreamingContext(conf, Seconds(2))
     
-    //initialize the topic
-    val topics = Array("sprktpk")
+    //initialize the topic name ---- 4
+    val topics = Array("consumption_method_check")
     
-    //initialize kafka params   
+    //initialize kafka params ---- 5
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "group.id" -> "consumer3",
       "auto.offset.reset" -> "earliest"
     )
-      
-    //Kafka utils  - combining streamimg context, topics and kafka params
+     
+    //RDD Streaming
+    //Kafka utils  - combining streamimg context, topics and kafka params. RDD/D Streaming ---- 6
     val stream = KafkaUtils.createDirectStream[String, String](
         ssc,
         PreferConsistent,
         Subscribe[String, String](topics, kafkaParams)
        )
-       
+    
     //fetching value and printing it, Kafka gives out a key value pair while the value section contains the stream data
-    stream.map(record => (record.value)).print()
+    val streamdata = stream.map(x => (x.value)) //RDD operation
+    
+    //To process streamed data, you have to use foreachRDD()
+    streamdata.foreachRDD(x=>
+      if(!x.isEmpty()){
+        val df = x.toDF("value") //converting RDD to dataframe
+        val df_withtime = df.withColumn("timestamp",current_timestamp) //Creating a new column and attaching time stamp
+        df_withtime.show(false) //prints out the CLI producer input/Nifi input with time stamp
+      }
+    
+    )
+    
     
     //start the stream
     ssc.start()
